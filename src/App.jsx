f6e4b676e6116f97e6272317f90658e612e93b00
abu0306 <2261983469@qq.com>
2023-04-22 16:21:55 +0800

    feat: fix display error

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
 
   useEffect(() => {
     const nodeConnection = new AleoNetworkClient("http://192.168.200.25:3030");
-
+	  const nodeRoot = 'zkgaming_blackjack.aleo';
     let stage = new createjs.Stage(canvasRef.current);
 		var mask = {
 		maskDom: document.getElementById('mask'),
@@ -72,6 +72,7 @@ function App() {
 		  },
 		  startContainer: undefined,
 		  buttons: [
+			// new Button('Hit', '#fff', 100, 100, () =>game.init_test()),
 			  new Button('Hit', '#fff', 100, 100, () => player.hit()),
 			  new Button('Stand', '#fff', 200, 100, () => player.stand()),
 			  new Button('Go', '#fff', 935, -430, () => game.go()),
@@ -211,8 +212,6 @@ function App() {
 			  }
 		  },
 
-  
-
 		  start: function () {
 			  const player_name = player.name.value;
 			  player.name.text = new createjs.Text(player_name, '30px Arial', '#fff');
@@ -235,68 +234,13 @@ function App() {
 				  player.betted = true;
 				  this.message.text.text = '';
 				  //todo 
-				  mask.show(`./target/release/snarkos developer execute --private-key ${player.account_key} --broadcast "http://127.0.0.1:3030/testnet3/transaction/broadcast" --query "http://127.0.0.1:3030"  zkgaming_blackjack1.aleo request_start`);
+				  mask.show(`./target/release/snarkos developer execute --private-key ${player.account_key} --broadcast "http://127.0.0.1:3030/testnet3/transaction/broadcast" --query "http://127.0.0.1:3030"  ${nodeRoot} request_start`);
 				  mask.mask_init();
 				  record_state.game_init();
 			  }
 			  else if (!player.dealt)
 				  game._alert(messages.warning.bet);
 		  },
-
-		//   stand_test() {
-		// 	  const outputs =[
-		// 		{
-		// 			"type": "public",
-		// 			"id": "5249286986330358071933528436165458608601628145137060698637977125911621899048field",
-		// 			"value": "aleo1rhgdu77hgyqd3xjj8ucu3jj9r2krwz6mnzyd80gncr5fxcwlh5rsvzp9px"
-		// 		},
-		// 		{
-		// 			"type": "public",
-		// 			"id": "5878944275294492537112304146515818798595698766335785721974572781047519863939field",
-		// 			"value": "67915324440375155815971772689629909761u128"
-		// 		},
-		// 		{
-		// 			"type": "public",
-		// 			"id": "2640077906399257265155023005902261965007333700966802000299014909981012529511field",
-		// 			"value": "16262385143592631044u64"
-		// 		},
-		// 		{
-		// 			"type": "public",
-		// 			"id": "7057815883212861856057055560644245377677404806708977636103338640747511033386field",
-		// 			"value": "4u8"
-		// 		}
-		// 	  ]
-		// 	   const obj = {
-		// 				  winner: outputs[0].value,
-		// 					gameId:outputs[2].value,
-		// 				  cards: outputs[1]?.value?.replace('u128',''),
-		// 				  next: outputs[3].value,
-							
-		// 			  }
-		// 			const deck_list = decode(obj.cards);
-		// 			const next = obj.next.replace('u8', '');
-		// 			console.log('---44deck_list', deck_list,next)
-		// 			player.stand_card(deck_list, obj.winner, Number(next));
-		//    },
-
-		// init_test() { 
-		// 	const catd_value = [];
-		// 	const player_first = {
-		// 		mainer: 'player',
-		// 		hidden: false,
-		// 		value: 2,
-		// 	};
-		// 	for (let i = 0; i < 4; i++) { 
-		// 		catd_value.push({
-		// 				  mainer: i < 2 ? 'player':'bank',
-		// 				  hidden:i === 2,
-        //                 value:i*3 + 1,
-                      
-		// 		})
-		// 	}
-		// 	  game.new(player_first,catd_value)
-		// },
-
 		end: function(){
 			game.dealtChipContainer?.removeAllChildren();
 			game.inProgress = false;
@@ -328,15 +272,6 @@ function App() {
 
         
       }
-			// setTimeout(function(){
-			// 	game.distributeCard('player',undefined,5);
-			// 	setTimeout(function(){
-			// 		game.distributeCard('bank',undefined,6);
-			// 		setTimeout(function(){
-			// 			game.distributeCard('bank', true,undefined,10);
-			// 		}, 750);
-			// 	}, 750);
-			// }, 750);
     },
    
 
@@ -363,15 +298,12 @@ function App() {
 
 		deckValue: function(deck){
 			var total = 0;
-			deck.forEach(function(card){
-				if(card.value >= 2 && card.value < 11)
-					total += card.value;
-				if(['J', 'Q', 'K'].includes(card.value))
+			deck.forEach(function (card) {
+				if(card.value >= 1 && card.value < 11)
+					total += Number(card.value);
+				if(card.value >=11)
 					total += 10;
-				if(card.value === 'A')
-					total += 11;
 			});
-
 			return total;
 		},
 
@@ -385,7 +317,7 @@ function App() {
 				suit: suits_num(number?.value_unit?.toString()),
 				hidden: false
 			};
-			console.log('=card==number3',canumberrd)
+			console.log('=card==number3',number)
 			if(hidden) card.hidden = true;
 
 			if(to === 'bank')
@@ -428,7 +360,8 @@ function App() {
 				player.cardsContainer.addChild(card1);
 				createjs.Tween.get(card1).to({x: 50 * player.deck.length, y: 100}, 750, createjs.Ease.getPowInOut(1));
 				player.cardsContainer.x -= 20;
-				
+				if(this.deckValue(player.deck) > 21)
+					player.lose();
 			}
 
 		},
@@ -554,7 +487,7 @@ function App() {
 		deck: [],
 		cardsContainer: undefined,
 		blackJack: false,
-
+		
 		play: function(cardList,winer,nextIndex){
 			if(player.doubled && player.deck.length > 2)
 				player.cardsContainer.children[2].image.src = imgs.cards.get(player.deck[2].suit, player.deck[2].value);
@@ -562,21 +495,41 @@ function App() {
 			if(this.deck.length === 2)
 				this.cardsContainer.children[1].image.src = imgs.cards.get(this.deck[1].suit, this.deck[1].value);
 
-			var total = game.deckValue(this.deck);
-			if (total < 17) {
-				// 依次给牌
-				const item_card_value = cardList[nextIndex];
-					console.log('====45', item_card_value,item_card_value.toString());
-				game.distributeCard('bank',undefined,item_card_value.toString());
-				if(game.deckValue(this.deck) < 17)
+
+			const item_card_value = cardList[nextIndex];
+			var bankScore = game.deckValue(bank.deck);
+			var playerScore = game.deckValue(player.deck);
+
+			console.log("=======bank play===========+++",bankScore,playerScore,winer,record_state.account.toString(),Number(item_card_value))
+			if (winer === record_state.account.toString()) {
+				/// player winner
+				if (bankScore + Number(item_card_value.toString()) <= playerScore) {
+					game.distributeCard('bank',undefined,item_card_value.toString());
 					setTimeout(() => bank.play(
 						cardList,winer,nextIndex+1
 					), 1000);
-				else
+				}
+				else {
 					game.check();
+				}
+			} else {
+				/// bank winner
+				if (bankScore + Number(item_card_value.toString()) < playerScore) {
+					game.distributeCard('bank',undefined,item_card_value.toString());
+					setTimeout(() => bank.play(
+						cardList,winer,nextIndex+1
+					), 1000);
+				} else if (bankScore + Number(item_card_value.toString()) >= playerScore && !this.blackJack) {
+					this.blackJack = true
+					game.distributeCard('bank',undefined,item_card_value.toString());
+					bank.play(
+						cardList,winer,nextIndex
+					)
+				}
+				else {
+					game.check();
+				}
 			}
-			else
-				game.check();
 		},
 
 	};
@@ -611,13 +564,12 @@ function App() {
 		chips: game.balanceChips(1000),
 
     	hit: function () {
-			mask.show(`./target/release/snarkos developer execute --private-key ${player.account_key} --broadcast "http://127.0.0.1:3030/testnet3/transaction/broadcast" --query "http://127.0.0.1:3030" zkgaming_blackjack1.aleo request_hit`);
+			mask.show(`./target/release/snarkos developer execute --private-key ${player.account_key} --broadcast "http://127.0.0.1:3030/testnet3/transaction/broadcast" --query "http://127.0.0.1:3030" ${nodeRoot} request_hit`);
 			mask.mask_init();
 		
 		},
 		
 		hit_card: function (card, isEnd) { 
-			this.betted = isEnd;
 			if(this.betted){
 				game.distributeCard(card.mainer,card.hidden,card.value);
 			}
@@ -626,14 +578,16 @@ function App() {
 		},
 
 		stand_card: function (cardList, winer, nextIndex) { 
+			console.log("===stand_card====",this.betted)
 			if(!this.betted)
-			return game._alert(messages.warning.bet);
+				return game._alert(messages.warning.bet);
+			
 			game.inProgress = true;
 			bank.play(cardList,winer,nextIndex);
 		},
 
 		stand: function () {
-			mask.show(`./target/release/snarkos developer execute --private-key ${player.account_key} --broadcast "http://127.0.0.1:3030/testnet3/transaction/broadcast" --query "http://127.0.0.1:3030" zkgaming_blackjack1.aleo request_stand`);
+			mask.show(`./target/release/snarkos developer execute --private-key ${player.account_key} --broadcast "http://127.0.0.1:3030/testnet3/transaction/broadcast" --query "http://127.0.0.1:3030" ${nodeRoot} request_stand`);
 			mask.mask_init();
 			
 		},
@@ -751,7 +705,7 @@ function App() {
     gameId: '',
     account: undefined,
    game_init: async function () { 
-      let currentHeight = 4628;
+      let currentHeight = 9380;
       while (true) {
       let latestBlock = (await nodeConnection.getLatestBlock().catch(err => {
         console.log(err);
@@ -773,17 +727,20 @@ function App() {
       currentBlock?.transactions?.forEach(transaction => {
         if (transaction.type === "execute") {
           transaction.execution.transitions.forEach((transition) => {
+
             if (transition.function === "exchange") {
+              console.log('=====45645665-currentHeight--',transition)
+
               // 解析 process_start_request 获取 用户 值
               let recordCiphertext = RecordCiphertext.fromString(transition.outputs[1].value)
               if (recordCiphertext.isOwner(this.account.viewKey())) {
                 let recordPlaintext = recordCiphertext.decrypt(this.account.viewKey())
                 let exchange = parsedNoNormalJsonString(recordPlaintext.toString())
+				console.log('---3,',exchange.owner,this.account.toString())
                 if (exchange.owner === this.account.toString() ) {
                   //设置游戏ID
 					this.gameId = exchange.id;
-					console.log('=====45645665-currentHeight--',currentHeight)
-					mask.show(`./target/release/snarkos developer execute --private-key ${player.account_key} --broadcast "http://127.0.0.1:3030/testnet3/transaction/broadcast" --query "http://127.0.0.1:3030" zkgaming_blackjack1.aleo set_part2_nonce   "${recordPlaintext.toString()}" ${parseInt(Math.random()*1000+ 1000)}u64`)
+					mask.show(`./target/release/snarkos developer execute --private-key ${player.account_key} --broadcast "http://127.0.0.1:3030/testnet3/transaction/broadcast" --query "http://127.0.0.1:3030" ${nodeRoot} set_part2_nonce   "${recordPlaintext.toString()}" ${parseInt(Math.random()*10000+ 5000)}u64`)
                     mask.mask_init();
 					//setGameId(exchange.id)
                 }
@@ -809,13 +766,13 @@ function App() {
 						player_first = {
 							mainer: 'player',
 							hidden:false,
-							value: marker_card.point.replace(/\u128/g,''),
+							value: marker_card.raw_point.replace(/\u128/g,''),
 						}
 					} else { 
 						 card_value.push({
 						  mainer: index >1 ? 'player':'bank',
 						  hidden:false,
-                        value:marker_card.point.replace(/\u128/g,''),
+                        value:marker_card.raw_point.replace(/\u128/g,''),
                       })
 					}
 					  }
@@ -828,7 +785,7 @@ function App() {
   
             if (transition.function === "process_hit_request") {
 				let out = parsedNoNormalJsonString(transition.outputs[1].value)
-				console.log('-out--process_hit_request',out)
+				console.log('-out--process_hit_request',out,this.gameId,record_state.account.toString())
               if (out.player === record_state.account.toString() && out.game_id === this.gameId) {
 				  let isEnd = transition.outputs[2].value
 				  const my_card = out;
@@ -836,7 +793,7 @@ function App() {
 				  player.hit_card({
 							mainer: 'player',
 							hidden:false,
-							value: my_card.point.replace(/\u128/g,''),
+							value: my_card.raw_point.replace(/\u128/g,''),
 						},isEnd)
                 // setMyCards(parsedNoNormalJsonString(out))
                 // setIsGameEnd(Boolean(isEnd))
@@ -860,12 +817,6 @@ function App() {
 					console.log('---stand---',{
                 
                 })
-                // SetStand({
-                //   winner: transition.outputs[0].value,
-                //   next: transition.outputs[3].value,
-                //   gameId:transition.outputs[2].value,
-                //   cards: transition.outputs[1].value
-                // })
               }
             }
           })
@@ -883,161 +834,6 @@ function App() {
 	  game.startScreen();
 	 // record_state.game_init()
    },[])
-
-
-  async function log() {
-    let account = new Account({ privateKey: "APrivateKey1zkpH6TQz2ksSEsBF4BqrL87gLrM6XhLBEZvNSWAWrd8DrCJ" });
-    console.log(account)
-    console.log(account.viewKey())
-    console.log(account.toString())
-    let nodeConnection = new AleoNetworkClient("http://192.168.200.25:3030");
-    let currentHeight = 223;
-    let currentBlock = (await nodeConnection.getBlock(currentHeight).catch(err => {
-      console.log(err);
-    }));
-    console.log("Syncing block", currentBlock.header.metadata.height);
-    currentBlock.transactions?.forEach(transaction => {
-      if (transaction.type === "execute") {
-        transaction.execution.transitions.forEach((transition) => {
-          if (transition.function === "exchange") {
-            // 解析 process_start_request 获取 用户 值
-            let recordCiphertext = RecordCiphertext.fromString(transition.outputs[1].value)
-            if (recordCiphertext.isOwner(account.viewKey())) {
-              let recordPlaintext = recordCiphertext.decrypt(account.viewKey())
-              let exchange = parsedNoNormalJsonString(recordPlaintext.toString())
-              if (exchange.owner === account.toString()) {
-                //设置游戏ID
-                setGameId(exchange.id)
-              }
-            }
-          }
-          if (transition.function === "start_game") {
-            // const outSting = transition.outputs[3].value.replace(/(\w+)\s*:/g, '"$1":').replace(/:\s*([\w\d]+)/g, ': "$1"')
-            let out = parsedNoNormalJsonString(transition.outputs[3].value)
-            console.log(out,account.toString())
-            if (out.player === account.toString() && out.game_id === gameId) {
-              transition.outputs.forEach((value,index) => {
-                if (index > 0) {
-                  if (index==1) {
-                    setMakerCards(parsedNoNormalJsonString(value.value))
-                  }
-                  if (index>1) {
-                    setMyCards(parsedNoNormalJsonString(value.value))
-                  }
-                }
-              })
-            }        
-          }
-
-          if (transition.function === "process_hit_request") {
-           let out = parsedNoNormalJsonString(transition.outputs[1].value)
-            if (out.player === account.toString() && out.game_id === gameId) {
-              let isEnd = parsedNoNormalJsonString(transition.outputs[2].value)
-              setMyCards(parsedNoNormalJsonString(out))
-              setIsGameEnd(Boolean(isEnd))
-            }
-          }
-
-          if (transition.function === "process_stand_request") {
-            if (transition.outputs[2]=== gameId) {
-              SetStand({
-                winner: transition.outputs[0].value,
-                next: transition.outputs[3].value,
-                gameId:transition.outputs[2].value,
-                cards: transition.outputs[1].value
-              })
-            }
-          }
-        })
-      }
-    });
-  }
-
-  // log()
-
-  async function logs() {
-    let nodeConnection = new AleoNetworkClient("http://192.168.200.25:3030");
-    console.log(nodeConnection)
-    let account = new Account({ privateKey: "APrivateKey1zkp8CZNn3yeCseEtxuVPbDCwSyhGW6yZKUYKfgXmcpoGPWH" });
-    let currentHeight = 100;
-    while (true) {
-      let latestBlock = (await nodeConnection.getLatestBlock().catch(err => {
-        console.log(err);
-      }))
-      console.log(latestBlock.header.metadata.height)
-      if (currentHeight >= latestBlock.header.metadata.height) {
-        await sleep(1000 * 15);
-        continue;
-      }
-      currentHeight += 1;
-      
-      let currentBlock = (await nodeConnection.getBlock(currentHeight).catch(err => {
-        console.log(err);
-      }));
-      console.log("Syncing block", currentBlock.header.metadata.height);
-      currentBlock.transactions?.forEach(transaction => {
-        if (transaction.type === "execute") {
-          transaction.execution.transitions.forEach((transition) => {
-            if (transition.function === "exchange") {
-              // 解析 process_start_request 获取 用户 值
-              let recordCiphertext = RecordCiphertext.fromString(transition.outputs[1].value)
-              if (recordCiphertext.isOwner(account.viewKey())) {
-                let recordPlaintext = recordCiphertext.decrypt(account.viewKey())
-                let exchange = parsedNoNormalJsonString(recordPlaintext.toString())
-                if (exchange.owner === account.toString()) {
-                  //设置游戏ID
-                  setGameId(exchange.id)
-                }
-              }
-            }
-            if (transition.function === "start_game") {
-              // const outSting = transition.outputs[3].value.replace(/(\w+)\s*:/g, '"$1":').replace(/:\s*([\w\d]+)/g, ': "$1"')
-              let out = parsedNoNormalJsonString(transition.outputs[3].value)
-              console.log(out,account.toString())
-              if (out.player === account.toString() && out.game_id === gameId) {
-                transition.outputs.forEach((value,index) => {
-                  if (index > 0) {
-                    if (index == 1) {
-                      makerCards.push(parsedNoNormalJsonString(value.value))
-                      //makerCards.point
-                      setMakerCards(makerCards)
-                    }
-                    if (index>1) {
-                      setMyCards(parsedNoNormalJsonString(value.value))
-                    }
-                  }
-                })
-              }        
-            }
-  
-            if (transition.function === "process_hit_request") {
-             let out = parsedNoNormalJsonString(transition.outputs[1].value)
-              if (out.player === account.toString() && out.game_id === gameId) {
-                let isEnd = parsedNoNormalJsonString(transition.outputs[2].value)
-                setMyCards(parsedNoNormalJsonString(out))
-                setIsGameEnd(Boolean(isEnd))
-              }
-            }
-  
-            if (transition.function === "process_stand_request") {
-              if (transition.outputs[2]=== gameId) {
-                SetStand({
-                  winner: transition.outputs[0].value,
-                  next: transition.outputs[3].value,
-                  gameId:transition.outputs[2].value,
-                  cards: transition.outputs[1].value
-                })
-              }
-            }
-          })
-        }
-      });
-    }
-  }
-  
-  // useEffect(() => {
-  //   logs
-  // },[])
 
   return (
   <>
